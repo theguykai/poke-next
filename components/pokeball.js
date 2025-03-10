7c7968ac70467b30151f05a98ef36dc318379ce6
Kaidhn Shearer <kaidhnshearer@gmail.com>
2025-03-11 09:50:22 +1000

    extract throw button out to its own component

--- components/pokeball.js
+++ components/pokeball.js
@@ -6,6 +6,7 @@ import Image from 'next/image';
 
 import pokeballs from './images/pokeball-links/links';
 import PokeballImage from './pokeball-image';
+import ThrowButton from './throw-button';
 import { getRandomPokemon } from "../app/lib/pokemon";
 
 export default function Pokeball() {
@@ -43,8 +44,8 @@ export default function Pokeball() {
     return (
         <div className='flex flex-col items-center justify-center gap-10 my-10'>
             <PokeballImage src={pokeballImg} alt={pokeballAlt} position={position} width="300" height="300"/>
-            <button className='py-4 px-2 bg-amber-400 text-amber-50 font-bold text-7xl rounded-md hover:rounded-xl hover:bg-amber-200 hover:font-8xl hover:cursor-pointer  duration-300' onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}
-            onClick={handleClick}>Throw!</button>
+            <ThrowButton onClick={handleClick} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}
+            />
         </div>
     )
 }
