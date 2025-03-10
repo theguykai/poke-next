661ac008fa3530ef02e80be8d2645d821cb6f76d
Kaidhn Shearer <kaidhnshearer@gmail.com>
2025-03-11 09:37:20 +1000

    add lib async function to GET random pokemon off of Pokemon API. add logic to pokeball component

---
+++ app/lib/pokemon.js
@@ -0,0 +1,17 @@
+export async function getRandomPokemon(num) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`);
+
+        if (!response.ok) {
+            throw new Error('Data could not be found');
+        }
+
+        const pokemon = await response.json();
+        console.log(pokemon.name);
+        return pokemon;
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+}
--- components/pokeball.js
+++ components/pokeball.js
@@ -1,16 +1,23 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 
 import pokeballs from './images/pokeball-links/links';
+import { getRandomPokemon } from "../app/lib/pokemon";
 
 export default function Pokeball() {
     const [pokeballImg, setPokeballImg] = useState(pokeballs[0].src);
     const [pokeballAlt, setPokeballAlt] = useState(pokeballs[0].alt);
 
+    const [pokeNum, setPokeNum] = useState(null);
+
     const [position, setPosition] = useState("");
+
+    const getRandomNumber = () => {
+        return Math.floor(Math.random() * 1025) + 1
+    }
 
     const handleMouseOver = () => {
         setPosition("-rotate-20");
@@ -23,7 +30,14 @@ export default function Pokeball() {
     const handleClick = () => {
         setPokeballImg(pokeballs[1].src);
         setPokeballAlt(pokeballs[1].alt);
+        setPokeNum(getRandomNumber());
+        console.log(getRandomNumber())
     }
+
+    useEffect(() => {
+        if (pokeNum)
+        getRandomPokemon(pokeNum);
+    }, [pokeNum])
 
     return (
         <div className='flex flex-col items-center justify-center gap-10 my-10'>
