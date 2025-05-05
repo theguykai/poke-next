305fb618d2ed8b3dba088146cbc488d0ea9a4401
Kaidhn Shearer <kaidhnshearer@gmail.com>
2025-05-05 20:17:11 +1000

    add pokeapi link footer

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 import Pokeball from "@/components/pokeball"
 
@@ -8,6 +9,10 @@ export default function Home() {
     <div>
       <main className="bg-gradient-to-br from-blue-500 to-yellow-100 flex flex-col items-center justify-center min-h-screen py-2">
         <Pokeball />
+        <footer>
+          <p className="text-gray-500 text-sm">Powered by</p>
+          <Link href="https://pokeapi.co/">PokeAPI v2</Link>
+        </footer>
       </main>
     </div>
   );
