f07fc6c7dc07032ef3ee4f21193a4cfffb7b7feb
Kaidhn Shearer <kaidhnshearer@gmail.com>
2025-05-07 12:22:50 +1000

    add hover effect for pokeapi link, add github link

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,7 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 import Pokeball from "@/components/pokeball"
 
 
@@ -9,9 +12,14 @@ export default function Home() {
     <div>
       <main className="bg-gradient-to-br from-blue-500 to-yellow-100 flex flex-col items-center justify-center min-h-screen py-2">
         <Pokeball />
-        <footer>
-          <p className="text-gray-500 text-sm">Powered by</p>
-          <Link href="https://pokeapi.co/">PokeAPI v2</Link>
+        <footer className="flex flex-col items-center justify-center gap-2">
+          <div>
+            <p className="text-gray-500 text-sm text-center">Powered by</p>
+            <Link href="https://pokeapi.co/" className="hover:text-yellow-500">PokeAPI v2</Link>
+          </div>
+          <Link href="https://github.com/theguykai/poke-next/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="3x" className="h-6 w-6 hover:text-blue-500" />
+          </Link>
         </footer>
       </main>
     </div>
